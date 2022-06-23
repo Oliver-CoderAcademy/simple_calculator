5f192fc9af1e753a384dae29593832702a03cfae
Hauteclere <oliver.f.lavers@gmail.com>
2022-06-23 10:40:46 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def my_function(some_parameter):
+    return f"{some_parameter}"
+
+print(my_function("some_other_argument"))
+
+# The function definition needs to give a name to the expected inputs. 
+# These names are *parameters*
+
+# When the function is called, the inputs that are given are called the *arguments*
